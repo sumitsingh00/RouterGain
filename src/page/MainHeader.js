2f42318da933c51f219d2fe9ed57dd681021dcf4
sumitsingh00 <sumit00134@gmail.com>
2024-03-22 01:17:28 +0530

    mainhadder

--- src/page/MainHeader.js
+++ src/page/MainHeader.js
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom'
 const MainHeader = () => {
     return (
         <div>
+          {/* <h1>HI THIS IS MAIN HADER</h1> */}
         <Outlet/>
           
         </div>
